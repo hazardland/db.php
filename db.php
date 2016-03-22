8cfb01cfb09c93f8d02967777c084bbddcdf9ad8
unknown <hazardland@gmail.com>
2016-03-23 01:39:30 +0400

    first commit

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
-    Copyright (c) 2014 hazardland
+    Copyright (c) 2014-2016 hazardland
 
     Permission is hereby granted, free of charge, to any person obtaining a copy
     of this software and associated documentation files (the "Software"), to deal
